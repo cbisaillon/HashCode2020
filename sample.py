52fcdc33df331d4e9cff0c445f6c33a0895aefec
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2020-02-20 13:40:55 -0500

    Solution class.

--- sample.py
+++ sample.py
@@ -6,4 +6,4 @@ def loadQuestionSamples() -> [Question]:
     return [Question('sample_question', days_to_scan=10, book_scores=[1,2,3,4], libraries=[(1, 1, [1, 2, 3]), (1, 2, [0,2,3])])]
 
 def loadSolutionSamples() -> [Solution]:
-    return [Solution('sample_solution')]
+    return [Solution('sample_solution', libraries=[Library(id=0, book_per_day=1, signup_time=1, books=[1, 2])], books_from_libary=[[Book(1, 10)]])]
