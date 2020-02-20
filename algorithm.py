ce3145946447b3260e07b992be4d0e263c9b037b
Clement <clement.bisaillon@hotmail.com>
2020-02-20 13:47:49 -0500

    algorithm

--- algorithm.py
+++ algorithm.py
@@ -14,12 +14,26 @@ def solve(question: Question) -> Solution:
     libraries.sort()
     totalDays = 0
 
+    librariesOut = []
+    booksOut = []
+
     # signup all the libraries
-    for library in libraries:
+    for index, library in enumerate(libraries):
+        totalDaysShip = library.signup_time
+        librariesOut.append(library)
+        booksOut.append([])
+        for book in library.books:
+            totalDaysShip += 1 / library.books_per_day
+            booksOut[index].append(book)
+
+            if totalDaysShip > question.days_to_scan:
+                break
 
         totalDays += library.signup_time
+        if(totalDays > question.days_to_scan):
+            break
 
-    return Solution(file_name=question.file_name)
+    return Solution(file_name=question.file_name, libraries=librariesOut, books_from_libary=booksOut)
 
 def solveAll(questions: [Question]) -> [Solution]:
     solutions = []
